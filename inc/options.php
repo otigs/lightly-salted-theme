ee8ddcc8c462564fc6294c4f90dfb60eac248921
Ollie Tigwell <ollie@lightlysalted.agency>
2026-02-01 23:24:35 +0000

    Refactor options handling in options.php to ensure data integrity by adding checks for array types when merging component options and additional data.

--- inc/options.php
+++ inc/options.php
@@ -6,8 +6,9 @@ use Flynt\Utils\Options;
 
 add_filter('Flynt/addComponentData', function ($data, $componentName): array {
     $options = array_reduce(array_keys(Options::OPTION_TYPES), function ($carry, $optionType) use ($componentName): array {
-        return array_merge($carry, Options::get($optionType, $componentName));
+        $optionData = Options::get($optionType, $componentName);
+        return array_merge($carry, is_array($optionData) ? $optionData : []);
     }, []);
 
-    return array_merge($options, $data);
+    return array_merge($options, is_array($data) ? $data : []);
 }, 9, 2);
